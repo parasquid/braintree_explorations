06d52ddbb83e155888416a0c9412908b56ac4284
parasquid <tristan.gomez@gmail.com>
2016-09-28 05:52:02 +0800

    post transaction to braintree

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'dotenv-rails'
+  gem "awesome_print"
 end
 
 group :development do
--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -5,6 +5,17 @@ class BraintreeController < ApplicationController
   end
 
   def checkout
-    raise
+    nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
+    result = Braintree::Transaction.sale(
+      :amount => "10.00",
+      :payment_method_nonce => nonce_from_the_client,
+      :options => {
+        :submit_for_settlement => true
+      }
+    )
+    transaction = result.transaction
+    logger.ap transaction
+
+    redirect_to :root, flash: {notice: transaction && transaction.status}
   end
 end
